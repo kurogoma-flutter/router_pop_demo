d8586d96e27f6ae28221f08953a52c1bc905970a
Kurogoma939 <kan.sss.939@gmail.com>
2022-11-26 03:10:28 +0900

    左スワイプで戻る処理＋制御実装

--- lib/base_scaffold.dart
+++ lib/base_scaffold.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+///
+/// BaseScaffold
+///
+/// [title]は、アプリバーのタイトルとして使用されます。
+///
+/// [body]は、アプリバーの下に配置されます。
+///
+/// [leftSwipe]は、左スワイプで戻る処理を制御します。
+///
 class BaseScaffold extends StatelessWidget {
   const BaseScaffold({
     super.key,
     required this.title,
     required this.body,
+    this.leftSwipe = true,
   });
 
   final String title;
   final Widget body;
+  final bool leftSwipe;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +32,11 @@ class BaseScaffold extends StatelessWidget {
       child: GestureDetector(
         /// GoRouterのcanPop()がtrueの場合は、左スワイプで戻る
         onHorizontalDragEnd: (details) {
+          // 左スワイプ禁止の場合は、処理を抜ける
+          if (!leftSwipe) {
+            return;
+          }
+          // pop可能な場合は、左スワイプで戻る
           if (context.canPop() && details.primaryVelocity! > 12) {
             context.pop();
           }
